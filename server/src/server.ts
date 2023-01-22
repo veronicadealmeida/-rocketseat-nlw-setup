4578a9e5c141d0680c81b76acabdc3dd62530ec7
Veronica <almeida.vrc@gmail.com>
2023-01-22 18:21:45 -0300

    Created relations and seed

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,13 +8,7 @@ const prisma = new PrismaClient();
 app.register(cors); // permite que qualquer aplicação consuma os dados do backend
 
 app.get('/hello', async () => {
-  const habits = await prisma.habit.findMany({
-    where: {
-      title: {
-        startsWith: 'Beber',
-      },
-    },
-  });
+  const habits = await prisma.habit.findMany();
 
   return habits;
 });
