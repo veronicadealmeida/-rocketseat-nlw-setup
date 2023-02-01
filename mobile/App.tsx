75ed49a43636b2b194e8aaa543e8541dab46fbe3
Veronica <almeida.vrc@gmail.com>
2023-02-01 00:10:39 -0300

    Add locale

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import {
   Inter_800ExtraBold,
 } from '@expo-google-fonts/inter';
 
+import './src/lib/dayjs';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
 
